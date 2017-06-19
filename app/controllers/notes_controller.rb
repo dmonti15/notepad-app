3b28dea185ba60e5637a1dcf6ed00ec60326b940
Daniella Montinola <dmonti15@gmail.com>
2017-06-18 21:15:54 -0500

    Generate notes controller and add routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,30 @@
+class NotesController < ApplicationController
+
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  def find_note
+  end
+
+  def note_params
+  end
+  
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # get '/index' => 'welcome#index'
   get 'welcome/index'
 
   root 'welcome#index'
 
-  # get '/index' => 'welcome#index'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # "resources" adds all the CRUD elements you'd need
+  resources :notes
 end
